6961c09a81ae6911bd32e8c7622312ff5437882a
diyanqi <diyanqi07@gmail.com>
2022-11-18 19:24:50 +0800

    Initial Commit

---
+++ wwyy Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wwyy Watch App
+//
+//  Created by 翟彦棋 on 2022/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wwyy Watch App/wwyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwyyApp.swift
+//  wwyy Watch App
+//
+//  Created by 翟彦棋 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct wwyy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
