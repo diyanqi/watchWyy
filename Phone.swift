ca66eab2e2324af3ec1b6dcd433ed7867cd1aba5
diyanqi <diyanqi07@gmail.com>
2023-05-03 20:26:43 +0800

    update

---
+++ Phone.swift
@@ -0,0 +1,20 @@
+//
+//  Phone.swift
+//  wwyy
+//
+//  Created by 翟彦棋 on 2023/5/3.
+//
+
+import SwiftUI
+
+struct Phone: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Phone_Previews: PreviewProvider {
+    static var previews: some View {
+        Phone()
+    }
+}
